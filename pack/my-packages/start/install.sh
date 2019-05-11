178faa6b6fc965d2cead47b0f1c9118557219b85
Milan Suk <milan.suk@kiwi.com>
2019-05-11 20:32:22 +0200

    add install script

---
+++ pack/my-packages/start/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/christoomey/vim-tmux-navigator.git
+git clone https://github.com/christoomey/vim-tmux-navigator.git
+git clone https://github.com/elixir-editors/vim-elixir
+git clone https://github.com/vim-python/python-syntax.git
+git clone https://github.com/fatih/vim-go.git
